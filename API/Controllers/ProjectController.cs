088f9253c7ccd7117f18b9e77ff5baedaf70d226
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-10-24 11:25:56 +0300

    feat: create project controller endpoints

--- API/Controllers/ProjectController.cs
+++ API/Controllers/ProjectController.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Entities.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services.Abstract;
-using Services.Concrete;
 
 namespace API.Controllers;
 
@@ -21,5 +21,44 @@ public class ProjectController : BaseController
     public IActionResult GetAll()
     {
         return HandleServiceResult(_projectService.Get());
+    }
+
+    [HttpPost]
+    public IActionResult CreateProject([FromBody] ProjectPostDto data)
+    {
+        return HandleServiceResult(_projectService.Create(data));
+    }
+
+    [HttpPut]
+    public IActionResult UpdateProject([FromBody] ProjectPutDto data)
+    {
+        return HandleServiceResult(_projectService.Update(data));
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteProject(Guid id)
+    {
+        return HandleServiceResult(_projectService.Delete(id));
+    }
+
+    [HttpPut]
+    [Route("AddUser")]
+    public IActionResult AddUserToProject([FromQuery] Guid projectId, [FromQuery] Guid userId)
+    {
+        return HandleServiceResult(_projectService.AddUserToProject(projectId, userId));
+    }
+
+    [HttpPut]
+    [Route("RemoveUser")]
+    public IActionResult RemoveUserFromProject([FromQuery] Guid projectId, [FromQuery] Guid userId)
+    {
+        return HandleServiceResult(_projectService.RemoveUserFromProject(projectId, userId));
+    }
+
+    [HttpPut]
+    [Route("MakeAdmin")]
+    public IActionResult ChangeUserProjectAdmin([FromQuery] Guid projectId, [FromQuery] Guid userId)
+    {
+        return HandleServiceResult(_projectService.SetUserAsAdmin(projectId, userId, true));
     }
 }
