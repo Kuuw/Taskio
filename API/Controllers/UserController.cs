985cd879354424f0a2fc1e9d4b7f4d2d16975405
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-10-24 14:01:52 +0300

    feat: add GET and PUT endpoints to UserController

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,5 +1,28 @@
-﻿namespace API.Controllers;
+﻿using Entities.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Services.Abstract;
+using Services.Concrete;
+
+namespace API.Controllers;
 
 public class UserController : BaseController
 {
+    private readonly IUserService _userService;
+    
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return HandleServiceResult(_userService.Get());
+    }
+
+    [HttpPut]
+    public IActionResult UpdateUser([FromBody] UserPutDto data)
+    {
+        return HandleServiceResult(_userService.Update(data));
+    }
 }
