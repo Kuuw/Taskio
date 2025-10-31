c2c5f336f3b7019f5ff12c6dcf3c89424ec4aed1
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-10-31 11:18:55 +0300

    refactor(entities): remove password field from UserGetDto

--- Entities/DTO/UserGetDto.cs
+++ Entities/DTO/UserGetDto.cs
@@ -10,8 +10,6 @@ public partial class UserGetDto
 
     public string Email { get; set; } = null!;
 
-    public string Password { get; set; } = null!;
-
     public DateTime CreatedAt { get; set; }
 
     public DateTime UpdatedAt { get; set; }
