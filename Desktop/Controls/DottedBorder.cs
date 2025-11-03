dd189a9eb8c2023c629693d6a659ecf0b4f34a8a
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-11-03 13:55:23 +0300

    feat: add dashed border styling to UI elements

---
+++ Desktop/Controls/DottedBorder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace Desktop.Controls;
+
+public class DottedBorderDrawable : IDrawable
+{
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        canvas.StrokeColor = Colors.Gray;
+        canvas.StrokeSize = 2;
+        canvas.StrokeDashPattern = new float[] { 5, 5 };
+        canvas.DrawRoundedRectangle(dirtyRect.Inflate(-2, -2), 8);
+    }
+}
+
+public class DottedBorder : GraphicsView
+{
+    public DottedBorder()
+    {
+        Drawable = new DottedBorderDrawable();
+    }
+}
