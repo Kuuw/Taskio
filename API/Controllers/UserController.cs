d07b3ef28df019f598257b76564001fbe48008f4
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-11-07 10:20:37 +0300

    feat(api): secure UserController with authorization

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,9 +1,13 @@
 using Entities.DTO;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Services.Abstract;
 
 namespace API.Controllers;
 
+[ApiController]
+[Route("[controller]")]
+[Authorize]
 public class UserController : BaseController
 {
     private readonly IUserService _userService;
