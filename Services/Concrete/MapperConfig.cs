491ebb5ced496956440110dc5c7cd17b7af97382
Kuuw <67199397+Kuuw@users.noreply.github.com>
2025-11-07 10:08:47 +0300

    refactor(services): convert MapperConfig to AutoMapper Profile pattern

--- Services/Concrete/MapperConfig.cs
+++ Services/Concrete/MapperConfig.cs
@@ -1,21 +1,28 @@
-﻿using AutoMapper;
-using Services.Abstract;
+using AutoMapper;
 
 namespace Services.Concrete;
 
-public class MapperConfig : IMapperConfig
+public class MapperConfig : Profile
 {
-    public static Mapper InitializeAutomapper()
+    public MapperConfig()
     {
-        var config = new MapperConfiguration(cfg =>
-        {
-            cfg.AddUserMappings();
-            cfg.AddCategoryMappings();
-            cfg.AddProjectMappings();
-            cfg.AddTaskMappings();
-        });
+        CreateMap<Entities.Models.User, Entities.DTO.UserGetDto>();
+        CreateMap<Entities.DTO.UserPutDto, Entities.Models.User>();
 
-        var mapper = new Mapper(config);
-        return mapper;
+        CreateMap<Entities.Models.Category, Entities.DTO.CategoryGetDto>();
+        CreateMap<Entities.DTO.CategoryPostDto, Entities.Models.Category>();
+        CreateMap<Entities.DTO.CategoryPutDto, Entities.Models.Category>();
+
+        CreateMap<Entities.Models.Project, Entities.DTO.ProjectGetDto>();
+        CreateMap<Entities.DTO.ProjectPostDto, Entities.Models.Project>();
+        CreateMap<Entities.DTO.ProjectPutDto, Entities.Models.Project>();
+        CreateMap<Entities.Models.ProjectUser, Entities.DTO.ProjectUserGetDto>()
+            .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.User.FirstName))
+            .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.User.LastName))
+            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email));
+
+        CreateMap<Entities.Models.Task, Entities.DTO.TaskGetDto>();
+        CreateMap<Entities.DTO.TaskPostDto, Entities.Models.Task>();
+        CreateMap<Entities.DTO.TaskPutDto, Entities.Models.Task>();
     }
 }
